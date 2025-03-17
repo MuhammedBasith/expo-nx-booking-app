2add7f679b1f6ef0ee1a02090a925418f6800e44
Basith <70204001+MuhammedBasith@users.noreply.github.com>
2025-03-17 22:41:07 +0530

    add future config

--- apps/myapp/src/config.ts
+++ apps/myapp/src/config.ts
@@ -5,3 +5,15 @@ export const config = {
   },
 };
 
+// export const config = {
+//   features: {
+//     hotel: {
+//       module: '@myworkspace/hotel',
+//       version: '0.0.1'
+//     },
+//     flight: {
+//       module: '@myworkspace/flight',
+//       version: '0.0.1',
+//     },
+//   },
+// };
